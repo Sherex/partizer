4c541db23eb557d3f633b8aefaa4a1a7fb97a3d0
Sherex <ingar@i-h.no>
2020-10-12 17:28:42 +0200

    Update index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,22 @@
 import * as db from './db/db'
+import * as query from './db/queries'
 
 (async () => {
-  await db.setup()
-  const collections = await db.getCollections()
+  // await db.setup()
 
-  console.log(Object.keys(collections))
+  await query.insertShelves([
+    {
+      location_id: 1,
+      name: 'A'
+    }
+  ])
 
-  const part = await db.addPart({
-    name: 'NE555',
-    description: 'A 555 timer'
-  })
-  console.log(part)
-
-  console.log(await db.getParts())
+  await query.insertContainers([
+    {
+      shelf_id: 1,
+      name: 'A'
+    }
+  ])
 })().catch(error => {
   console.error(error)
 })
